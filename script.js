1a9cfacb1f41f4ebe985217084dc32e37602c91f
Rafaela Vitória Port <rafaelavitoriaport1590@gmail.com>
2023-08-25 20:37:09 -0300

    tentativa dois de api

--- script.js
+++ script.js
@@ -12,16 +12,16 @@ function mostrarPosts(posts){
     const containerPosts = document.getElementById('card');
     posts.forEach((post, index) => {
         const thumbImageSrc = post.thumbImage.startsWith('/') ? `https://api-rest-post-diegocandido.herokuapp.com${post.thumbImage}` : post.thumbImage;
-        const postHTML = `
-        <div id ="card" onclick="mostrarUmPost(${index})">
+        const postHTML = 
+        <div id ="card" onclick="cardgrande(${index})">
         <img id="card" src="${thumbImageSrc}" alt="${post.thumbImageAltText}">
-            <div id="cimaPost">
+            <div id="cardgrande">
                 <h2>${post.title}</h2>
                 <p>${post.description}</p>
                 <p>Por ${post.profileName}, ${post.postDate}</p>
             </div>
         </div>
-
-    }});
-}
-fetchPost();
+        })};
+        
+        //nao sei o que aconteceu
+        //nao consigo mais fechar chaves
